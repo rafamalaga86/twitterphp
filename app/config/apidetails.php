0d0598122a90765dee5ed1c2e66593aad1b74bdf
rafamalaga86 <rafamalaga86@gmail.com>
2015-05-09 14:27:46 +0200

    Extracting details out in a file

---
+++ app/config/apidetails.php
@@ -0,0 +1,12 @@
+<?php
+
+$settings = array(
+    'oauth_access_token' => "1359745544-fbLmg2Mr7xXGANpp3eCkluUjcqQ4MpK1yur5zkP",
+    'oauth_access_token_secret' => "D6qEa4vn3ssef7BaZSfDnEOtLn21D2TUZ7b1DAbeMPzpX",
+    'consumer_key' => "Gy9GHvSKBKGtSndZgUfBwmzan",
+    'consumer_secret' => "h5xp4MKIRv9BT6I4pHFQ5X7ltEdLErtFlN3Gg7zbOZHBJROdOK"
+);
+
+$url = 'https://api.twitter.com/1.1/statuses/user_timeline.json';
+$getfield = '?screen_name=twitterapi&count=5';
+$requestMethod = 'GET';
