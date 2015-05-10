9b0c2b20b01fd68e881a20101f9cd78bd1bb68af
rafamalaga86 <rafamalaga86@gmail.com>
2015-05-11 01:32:50 +0200

    Adding the method psot 3

--- app/public/js/answer.js
+++ app/public/js/answer.js
@@ -5,7 +5,6 @@ var id;
 $('.answer-button').on('click', function(){
 
 	id = $(this).data('ttid');
-	$('#answer-tweet-form').attr('action', 'http://testing.clickcreacion.com/twitterphp/app/scripts/answer.php?id=' + id);
 	$('#ttid').attr('value', id);
 });
 
