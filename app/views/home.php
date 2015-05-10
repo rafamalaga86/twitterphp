d7ca3090beccc67e22286f009ea78117d2376ced
rafamalaga86 <rafamalaga86@gmail.com>
2015-05-11 00:39:55 +0200

    Adding textarea

--- app/views/home.php
+++ app/views/home.php
@@ -81,20 +81,22 @@
 
         <!-- Modal -->
         <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-          <div class="modal-dialog">
+        <div class="modal-dialog">
             <div class="modal-content">
-              <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-              </div>
-              <div class="modal-body">
-                ...
-              </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Answer</button>
-              </div>
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                </div>
+                <form>
+                    <div class="modal-body">
+                            <textarea maxlength="140"></textarea>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Answer</button>
+                    </div>
+                </form>
             </div>
-          </div>
+        </div>
         </div>
 
 
