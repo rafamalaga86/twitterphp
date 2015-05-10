a7c22160ac8f37042506c24fc0c0d970153765d9
rafamalaga86 <rafamalaga86@gmail.com>
2015-05-10 17:10:14 +0200

    Sending POST data from favourite

--- app/controllers/favourite.php
+++ app/controllers/favourite.php
@@ -5,3 +5,21 @@ require_once('../Twitterphp.php');
 
 $app = new TwitterPHP($settings);
 $id = $_GET['id'];
+
+echo "Esto es: $id";
+
+
+$url = 'http://testing.clickcreacion.com/twitterphp/app/controllers/test.php';
+$data = ['id' => $id];
+
+$options = [
+    'http' => [
+        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+        'method'  => 'POST',
+        'content' => http_build_query($data),
+    ],
+];
+$context  = stream_context_create($options);
+$result = file_get_contents($url, false, $context);
+
+var_dump($result);
