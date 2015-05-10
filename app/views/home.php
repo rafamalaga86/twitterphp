2578602a707e8fc1fc1c082bd8f12103ec870bc0
rafamalaga86 <rafamalaga86@gmail.com>
2015-05-11 00:34:48 +0200

    Implementing replying tweets 2

--- app/views/home.php
+++ app/views/home.php
@@ -66,7 +66,7 @@
                     </div>
                     
                     <div class="action-row">
-                        <a class="action-link" href=""><i class="icon icon--reply"></i></a>
+                        <a class="action-link" href="#" data-toggle="modal" data-target="#myModal"><i class="icon icon--reply"></i></a>
                         <a class="action-link <?= isset($tweet['retweeted']) && $tweet['retweeted'] != 0 ? 'on' : '' ?>" href="http://testing.clickcreacion.com/twitterphp/app/scripts/retweet.php?id=<?= $tweet["tt_id"] ?>"><i class="icon icon--retweet"></i></p>
                         <a class="action-link <?= isset($tweet['favorited']) && $tweet['favorited'] != 0 ? 'on' : '' ?>" href="http://testing.clickcreacion.com/twitterphp/app/scripts/favourite.php?id=<?= $tweet["tt_id"] ?>"><i class="icon icon--favorite"></i></a>
                     </div>
@@ -79,25 +79,19 @@
         <!-- /.row -->
 
 
-        <!-- Button trigger modal -->
-        <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
-          Launch demo modal
-        </button>
-
         <!-- Modal -->
         <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
           <div class="modal-dialog">
             <div class="modal-content">
               <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title" id="myModalLabel">Modal title</h4>
               </div>
               <div class="modal-body">
                 ...
               </div>
               <div class="modal-footer">
                 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
+                <button type="button" class="btn btn-primary">Answer</button>
               </div>
             </div>
           </div>
@@ -112,6 +106,7 @@
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <script src="app/public/js/bootstrap.min.js"></script>
+    <script src="app/public/js/answer.js"></script>
 
 
 
